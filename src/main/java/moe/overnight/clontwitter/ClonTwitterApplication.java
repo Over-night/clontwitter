6842600527d33374424109d467efba099478a4f9
Over-night <hidden98716@gmail.com>
2025-06-17 20:41:25 +0900

    initial push

---
+++ src/main/java/moe/overnight/clontwitter/ClonTwitterApplication.java
@@ -0,0 +1,13 @@
+package moe.overnight.clontwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moe/overnight/clontwitter/ClonTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.overnight.clontwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
