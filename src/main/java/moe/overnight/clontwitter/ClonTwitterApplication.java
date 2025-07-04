29157a8039caf6968a5e6a0b8aa57245987c481b
Over-night <hidden98716@gmail.com>
2025-07-04 16:44:05 +0900

    feat: add userRepo

--- src/main/java/moe/overnight/clontwitter/ClonTwitterApplication.java
+++ src/main/java/moe/overnight/clontwitter/ClonTwitterApplication.java
@@ -1,8 +1,17 @@
 package moe.overnight.clontwitter;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "ClonTwitter API",
+				version = "v1",
+				description = "SNS 클론 API 명세서"
+		)
+)
 @SpringBootApplication
 public class ClonTwitterApplication {
 
---
+++ src/main/java/moe/overnight/clontwitter/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package moe.overnight.clontwitter.repository;
+
+import moe.overnight.clontwitter.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
